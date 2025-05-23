76e8860951996d8f96d7de1e4cac01cad0526beb
Чумаченко Артем Александрович (4104080) <chumachenko@dev.vtb.ru>
2025-05-23 14:17:05 +0300

    add api test

---
+++ src/main/java/com/achumachenko/jefferson/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.achumachenko.jefferson.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+@CrossOrigin(origins = "*")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Ok");
+    }
+}
