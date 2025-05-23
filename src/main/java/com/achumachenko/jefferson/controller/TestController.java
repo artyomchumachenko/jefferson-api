3504050ba88156a81e86cc184e1be5a9811fb0d0
Чумаченко Артем Александрович (4104080) <chumachenko@dev.vtb.ru>
2025-05-23 14:34:52 +0300

    test

--- src/main/java/com/achumachenko/jefferson/controller/TestController.java
+++ src/main/java/com/achumachenko/jefferson/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 @CrossOrigin(origins = "*")
 public class TestController {
-    // test 123
+    // test 123 123
     @GetMapping
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("Ok");
